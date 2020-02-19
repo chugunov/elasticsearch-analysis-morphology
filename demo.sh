881417e558d77af7bef69440ddd6a3f881d8b1d5
Alexandr Laba <support@broccolidev.ru>
2020-02-19 17:49:08 +0800

    7.6.0

--- demo.sh
+++ demo.sh
@@ -4,14 +4,14 @@ curl --header "Content-Type:application/json" -XPUT 'http://localhost:9200/ruste
     "settings": {
 		"analysis": {
 			"analyzer": {
-				"my_analyzer": {
+				"morphology_analyzer": {
 					"type": "custom",
 					"tokenizer": "standard",
-					"filter": ["lowercase", "russian_morphology", "english_morphology", "my_stopwords"]
+					"filter": ["lowercase", "russian_morphology", "english_morphology", "morphology_stopwords"]
 				}
 			},
 			"filter": {
-				"my_stopwords": {
+				"morphology_stopwords": {
 					"type": "stop",
 					"stopwords": "а,без,более,бы,был,была,были,было,быть,в,вам,вас,весь,во,вот,все,всего,всех,вы,где,да,даже,для,до,его,ее,если,есть,еще,же,за,здесь,и,из,или,им,их,к,как,ко,когда,кто,ли,либо,мне,может,мы,на,надо,наш,не,него,нее,нет,ни,них,но,ну,о,об,однако,он,она,они,оно,от,очень,по,под,при,с,со,так,также,такой,там,те,тем,то,того,тоже,той,только,том,ты,у,уже,хотя,чего,чей,чем,что,чтобы,чье,чья,эта,эти,это,я,a,an,and,are,as,at,be,but,by,for,if,in,into,is,it,no,not,of,on,or,such,that,the,their,then,there,these,they,this,to,was,will,with"
 				}
@@ -20,12 +20,14 @@ curl --header "Content-Type:application/json" -XPUT 'http://localhost:9200/ruste
 	}
 }' && echo
 curl --header "Content-Type:application/json" -XPUT 'http://localhost:9200/rustest/doc/_mapping' -d '{
-	"doc": {
-    	"properties" : {
-        	"body" : { "type" : "text", "analyzer" : "russian_morphology" },
-        	"text" : { "type" : "text", "analyzer" : "my_analyzer" }
-    	}
-	}
+
+   "_all": {
+     "enabled": false
+   },
+    "properties" : {
+        "body" : { "type" : "text", "analyzer" : "russian_morphology" },
+        "text" : { "type" : "text", "analyzer" : "my_analyzer" }
+    }
 }' && echo
 curl --header "Content-Type:application/json" -XPUT 'http://localhost:9200/rustest/doc/1' -d '{"body": "У московского бизнесмена из автомобиля украли шесть миллионов рублей "}' && echo
 curl --header "Content-Type:application/json" -XPUT 'http://localhost:9200/rustest/doc/1' -d '{"body": "У московского бизнесмена из автомобиля украли шесть миллионов рублей "}' && echo
